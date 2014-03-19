ca66f9012bd4f5af2405e689cf186ad6b218a5ab
Kevin Cameron <kevin.2008.cameron@gmail.com>
2014-03-19 10:51:19 -0800

    Select you can now select nodes and the arrows point to the first connected node... multiple node is not working, will fix tomorrow.

--- ApocalypseWar/Assets/Standard Assets/Character Controllers/Sources/Scripts/ControlPoint.cs
+++ ApocalypseWar/Assets/Standard Assets/Character Controllers/Sources/Scripts/ControlPoint.cs
@@ -5,20 +5,36 @@ using System.Collections.Generic;
 public class ControlPoint : MonoBehaviour {
 
 	private enum ownerControl{Friendly, Neutral, Enemy};
+	private static bool oneSelected = false;
 
 	private GameObject c_object;
+	private GameObject c_arrow;
 	private GameObject c_light;
-	private static bool _isSelected;
+	private bool _isSelected;
+	private bool firstDelay;
 	private float SelectedLightIntensity;
 	private float BaseLightIntensity;
 
 	[SerializeField]ownerControl controlOwner;
 
 	[SerializeField]private List<GameObject> connectedPoints;
+	[SerializeField]private List<GameObject> arrows;
+
 	// Use this for initialization
 	void Start () {
 		c_object = this.gameObject;
 		c_light = this.transform.FindChild ("c_Light").gameObject;
+		c_arrow = this.transform.FindChild ("c_arrow").gameObject;
+		ArrowScript arrowS = (ArrowScript)c_arrow.GetComponent (typeof(ArrowScript));
+		arrowS.initilize (connectedPoints[0].transform.position, this.transform.position);
+		arrows.Add (c_arrow);
+		for(int i = 1; i != connectedPoints.Count; ++i)
+		{
+			GameObject newArrow = GameObject.Instantiate(c_arrow, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+			arrowS = (ArrowScript)newArrow.GetComponent (typeof(ArrowScript));
+			arrowS.initilize (connectedPoints[i].transform.position, this.transform.position);
+			arrows.Add(newArrow);
+		}
 		BaseLightIntensity = c_light.light.intensity;
 		SelectedLightIntensity = BaseLightIntensity + 5;
 		controlOwner = ownerControl.Neutral;
@@ -51,9 +67,10 @@ public class ControlPoint : MonoBehaviour {
 
 		c_light.light.intensity = (_isSelected) ? SelectedLightIntensity : BaseLightIntensity;
 
-		if (Input.GetMouseButton (0)) {
-				}
-			//checkSelected();
+		if (Input.GetMouseButton(0)) {
+			checkSelected();
+		}
+			
 	}
 	void checkSelected()
 	{
@@ -62,17 +79,28 @@ public class ControlPoint : MonoBehaviour {
 		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 
 		if (_isSelected)
-						_isSelected = false;
+			_isSelected = false;
 
-		if (Physics.Raycast(ray, out hit)){
-			if(hit.transform.gameObject.tag == "ControlPoint")
-			{}}
+		if (Physics.Raycast(ray, out hit))
+		{
+			if(hit.transform.gameObject.Equals(this.gameObject))
+			{
+				_isSelected = true;
+			}
+		}
 
 	}
 
-	void OnMouseDown(){
-		_isSelected = !_isSelected;
-
-	}
-
+	/*void OnMouseDown(){
+		firstDelay = true;
+		if(!oneSelected){
+			_isSelected = true;
+			oneSelected = true; // Static valuse to limit 1 object selected at a time
+		}
+		else if(oneSelected && _isSelected)
+		{
+			_isSelected = false;
+			oneSelected = false;
+		}
+	}*/
 }
---
+++ ApocalypseWar/Assets/Standard Assets/MyScripts/ArrowScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowScript : MonoBehaviour {
+
+	private bool _isSelected;
+	// Use this for initialization
+	void Start () {
+		_isSelected = false;
+	}
+
+	public void initilize(Vector3 pointAt, Vector3 pointFrom)
+	{
+		float scale = 3.5f;
+		//Vector3 direction = pointFrom - pointAt;
+		Vector3 direction = pointAt - pointFrom;
+		direction = direction.normalized;
+		transform.localPosition = new Vector3(scale*direction.x, transform.position.y, scale*direction.z);
+		//transform.position.z += direction.z;
+		transform.LookAt (pointAt);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		_isSelected = !_isSelected;
+	}
+}
