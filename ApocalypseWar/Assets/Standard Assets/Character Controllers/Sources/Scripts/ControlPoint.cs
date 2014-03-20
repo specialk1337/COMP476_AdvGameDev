29787c796eecd032fba0dcc890690de08382b1c1
jallarzie <jallarzie@gmail.com>
2014-03-20 19:20:23 -0400

    Fix a null pointer bug

--- ApocalypseWar/Assets/Standard Assets/Character Controllers/Sources/Scripts/ControlPoint.cs
+++ ApocalypseWar/Assets/Standard Assets/Character Controllers/Sources/Scripts/ControlPoint.cs
@@ -34,10 +34,12 @@ public class ControlPoint : MonoBehaviour {
 		arrows.Add (c_arrow);
 		for(int i = 1; i < connectedPoints.Count; ++i)
 		{
-			GameObject newArrow = GameObject.Instantiate(c_arrow, new Vector3(0,0,0), Quaternion.identity) as GameObject;
-			arrowS = (ArrowScript)newArrow.GetComponent (typeof(ArrowScript));
-			arrowS.initilize (connectedPoints[i].transform.position, this.transform.position);
-			arrows.Add(newArrow);
+			if (connectedPoints[i] != null) {
+				GameObject newArrow = GameObject.Instantiate(c_arrow, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+				arrowS = (ArrowScript)newArrow.GetComponent (typeof(ArrowScript));
+				arrowS.initilize (connectedPoints[i].transform.position, this.transform.position);
+				arrows.Add(newArrow);
+			}
 		}
 		BaseLightIntensity = c_light.light.intensity;
 		SelectedLightIntensity = BaseLightIntensity + 5;
