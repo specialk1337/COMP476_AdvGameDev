2260b8cd17c77de00d12219882c16c3520407a86
SolidFastidious <davidhuang6492@gmail.com>
2014-04-06 15:20:48 -0400

    Flocking for spiders

---
+++ ApocalypseWar/Assets/Standard Assets/MyScripts/ControlFlock.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlFlock : MonoBehaviour {
+
+	public float minVelocity = 1;
+	public float maxVelocity = 8;
+	public float randomness = 1;
+	public int flockSize = 10;
+
+	public GameObject leaderPrefab;
+	public GameObject lackey;
+
+	public Vector3 flockCenter;
+	public Vector3 flockVelocity;
+
+	private GameObject[] units;
+	// Use this for initialization
+	void Start () {
+		units = new GameObject [flockSize];
+
+		for (int i = 0; i < flockSize; i++) {
+			Vector3 position  = new Vector3 (Random.value * collider.bounds.size.x, Random.value * collider.bounds.size.y, Random.value * collider.bounds.size.z) - collider.bounds.extents;
+
+			GameObject unit = Instantiate (leaderPrefab, transform.position, transform.rotation) as GameObject;
+			unit.transform.parent = transform;
+			unit.transform.localPosition = position;
+			unit.GetComponent<FlockUnit>().SetController (gameObject);
+			units[i] = unit;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 center = Vector3.zero;
+		Vector3 velocity = Vector3.zero;
+
+		foreach (GameObject unit in units) {
+			center = center + unit.transform.localPosition;
+			velocity = velocity + unit.rigidbody.velocity;
+		}
+
+		flockCenter = flockCenter / (flockSize);
+		flockVelocity = flockVelocity / (flockSize);
+	}
+}
---
+++ ApocalypseWar/Assets/Standard Assets/MyScripts/FlockUnit.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlockUnit : MonoBehaviour {
+	private GameObject Controller;
+	private bool start = false;
+	private float minVelocity;
+	private float maxVelocity;
+	private float randomness;
+	private GameObject lackey;
+	
+	void Start ()
+	{
+		StartCoroutine ("Steer");
+	}
+	
+	IEnumerator Steer ()
+	{
+		while (true)
+		{
+			if (start)
+			{
+				rigidbody.velocity = rigidbody.velocity + Calc () * Time.deltaTime;
+				
+				// enforce minimum and maximum speeds for the boids
+				float speed = rigidbody.velocity.magnitude;
+				if (speed > maxVelocity)
+				{
+					rigidbody.velocity = rigidbody.velocity.normalized * maxVelocity;
+				}
+				else if (speed < minVelocity)
+				{
+					rigidbody.velocity = rigidbody.velocity.normalized * minVelocity;
+				}
+			}
+			
+			float waitTime = Random.Range(0.3f, 0.5f);
+			yield return new WaitForSeconds (waitTime);
+		}
+	}
+	
+	private Vector3 Calc ()
+	{
+		Vector3 randomize = new Vector3 ((Random.value *2) -1, (Random.value * 2) -1, (Random.value * 2) -1);
+		
+		randomize.Normalize();
+		ControlFlock controlFlock = Controller.GetComponent<ControlFlock>();
+		Vector3 flockCenter = controlFlock.flockCenter;
+		Vector3 flockVelocity = controlFlock.flockVelocity;
+		Vector3 follow = controlFlock.transform.localPosition;
+		
+		flockCenter = flockCenter - transform.localPosition;
+		flockVelocity = flockVelocity - rigidbody.velocity;
+		follow = follow - transform.localPosition;
+		
+		return (flockCenter + flockVelocity + follow * 2 + randomize * randomness);
+	}
+	
+	public void SetController (GameObject theController)
+	{
+		Controller = theController;
+		ControlFlock controlFlock = Controller.GetComponent<ControlFlock>();
+		minVelocity = controlFlock.minVelocity;
+		maxVelocity = controlFlock.maxVelocity;
+		randomness = controlFlock.randomness;
+		lackey = controlFlock.lackey;
+		start = true;
+	}
+}
