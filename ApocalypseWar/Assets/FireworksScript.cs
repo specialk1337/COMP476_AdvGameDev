5c8c43badd81517e099358d6f7bcd9eecad48095
ncik <nick@constantinidis.ca>
2014-04-16 03:59:18 -0400

    Added fireworks when a control point is captured

---
+++ ApocalypseWar/Assets/FireworksScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireworksScript : MonoBehaviour {
+
+	public float deathDelay;
+	private float deathCounter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		deathCounter += Time.deltaTime;
+		if (deathCounter >= deathDelay) {
+			Destroy(gameObject);
+		}
+	}
+}
