5fc3de76b6ff524d47012a569aac3b4d881b0422
David Zile Huang <davidhuang6492@gmail.com>
2014-04-18 21:22:30 -0400

    I added some GUI, intro and credits.
    Can someone configure the amount of bats and spiders. My laptop couldn't
    handle it in the game.  I have imported a gui skin, it is not big.

---
+++ ApocalypseWar/Assets/Standard Assets/MyScripts/Credits.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+	public Rect button = new Rect(50,50,100,65);
+	public Rect buttonCred = new Rect(180,100, 100, 40);
+
+	public string teams = "Kevin Cameron\n Nicholas Constantinidis\n Audrey Paiement\n David Huang";
+
+	public string LoadIntro = "Intro";
+	
+	public GUISkin guiSkin;
+	
+	private void OnGUI(){
+		GUI.skin = guiSkin;
+		//GUI.backgroundColor = Color.clear;
+		GUI.Box(buttonCred, teams);
+		if (GUI.Button(button, "Go Back"))
+			Application.LoadLevel(LoadIntro);
+	
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ApocalypseWar/Assets/Standard Assets/MyScripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public Rect button = new Rect(50,50,100,65);
+	public Rect buttonCred = new Rect(180,100, 100, 40);
+
+	public string LoadScreen = "WarZone2";
+	public string LoadCredit = "Credits";
+
+	public GUISkin guiSkin;
+
+	private void OnGUI(){
+		GUI.skin = guiSkin;
+		//GUI.backgroundColor = Color.clear;
+		if (GUI.Button(button, "Start"))
+			Application.LoadLevel(LoadScreen);
+		if (GUI.Button(buttonCred, "Credits"))
+			Application.LoadLevel(LoadCredit);
+	}
+}
